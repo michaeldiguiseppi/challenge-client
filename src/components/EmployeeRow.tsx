c4b35836f780048fec18808c3e1b2ac08b540411
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2021-04-28 19:43:02 -0600

    Update styling and imports

--- src/components/EmployeeRow.tsx
+++ src/components/EmployeeRow.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import type { Employee } from "./types";
 import { Link } from "react-router-dom";
-// import { EmployeeContext } from "../App";
 import "./stylesheets/EmployeeRow.css";
 
 type Props = {
